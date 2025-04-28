e617c4b72209c47181861909ec7b3ca2d57ed541
Domi <Dominik.janser@mail.com>
2025-04-28 16:53:53 +0200

    part 3 done

--- client/main.gd
+++ client/main.gd
@@ -2,27 +2,48 @@ extends Node
 
 const packets := preload("res://packets.gd")
 
+@onready var _log: Log = $Log
+
+var client_id: int
+@onready var _line_edit: LineEdit = $LineEdit
+
 func _ready() -> void:
 	WS.connected_to_server.connect(_on_ws_connected_to_server)
 	WS.connection_closed.connect(_on_ws_connection_closed)
 	WS.packet_received.connect(_on_ws_packet_received)
 	
-	print("Connecting to server...")
+	_log.info("Connecting to server...")
 	WS.connect_to_url("ws://127.0.0.1:8080/ws")
 
 func _on_ws_connected_to_server() -> void:
+	_log.success("Connected successfully")
+	
+func _on_ws_connection_closed() -> void:
+	_log.error("Connection closed")
+	
+func _on_ws_packet_received(packet: packets.Packet) -> void:
+	var sender_id := packet.get_sender_id()
+	if packet.has_id():
+		_handle_id_msg(sender_id, packet.get_id())
+	elif packet.has_chat():
+		_handle_chat_msg(sender_id, packet.get_chat())
+
+func _handle_id_msg(sender_id: int, id_msg: packets.IdMessage) -> void:
+	var client_id := id_msg.get_id()
+	_log.info("Received client ID: %d" % client_id)
+
+func _handle_chat_msg(sender_id: int, chat_msg: packets.ChatMessage) -> void:
+	_log.chat("Client %d" % sender_id, chat_msg.get_msg())
+
+
+func _on_line_edit_text_submitted(text: String) -> void:
 	var packet := packets.Packet.new()
 	var chat_msg := packet.new_chat()
-	chat_msg.set_msg("Hello, Golang!")
+	chat_msg.set_msg(text)
 	
 	var err := WS.send(packet)
 	if err:
-		print("Error sending packet")
+		_log.error("Error sending chat message")
 	else:
-		print("Sent packet")
-	
-func _on_ws_connection_closed() -> void:
-	print("Connection closed")
-	
-func _on_ws_packet_received(packet: packets.Packet) -> void:
-	print("Received packet from the server: %s" % packet)
+		_log.chat("You", text)
+	_line_edit.text = ""
--- server/internal/server/clients/websocket.go
+++ server/internal/server/clients/websocket.go
@@ -50,11 +50,19 @@ func (c *WebSocketClient) Id() uint64 {
 func (c *WebSocketClient) Initialize(id uint64) {
 	c.id = id
 	c.logger.SetPrefix(fmt.Sprintf("Client %d: ", c.id))
+	c.SocketSend(packets.NewId(c.id))
+	c.logger.Printf("Sent ID to client")
 }
 
 func (c *WebSocketClient) ProcessMessage(senderId uint64, message packets.Msg) {
-	c.logger.Printf("Received message: %T from client - echoing back...", message)
-	c.SocketSend(message)
+	if senderId == c.id {
+		// This message was sent by our own client, so broadcast it to everyone else
+		c.Broadcast(message)
+	} else {
+		// Another client interfacer passed this onto us, or it was broadcast from the hub,
+		// so forward it directly to our own client
+		c.SocketSendAs(message, senderId)
+	}
 }
 
 func (c *WebSocketClient) SocketSend(message packets.Msg) {
